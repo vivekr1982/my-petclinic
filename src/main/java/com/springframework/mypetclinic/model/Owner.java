a9765d3190443c9b11b88a1c52b2c2226d49ec3a
vivekr1982 <vivekr1982@gmail.com>
2019-01-06 13:35:47 +0530

    adding POJOs

---
+++ src/main/java/com/springframework/mypetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springframework.mypetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/springframework/mypetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springframework.mypetclinic.model;
+
+public class Vet extends Person {
+}
